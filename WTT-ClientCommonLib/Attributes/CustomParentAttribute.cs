b9344d73223b339836b82a0d01ac3a0fef44bbcb
peinwastaken <legokid173@gmail.com>
2026-04-24 22:24:45 +0300

    add xml comment and primary ctor for customparent attribute on client

--- WTT-ClientCommonLib/Attributes/CustomParentAttribute.cs
+++ WTT-ClientCommonLib/Attributes/CustomParentAttribute.cs
@@ -3,17 +3,16 @@ using System;
 
 namespace WTTClientCommonLib.Attributes;
 
+/// <summary>
+/// Declares a mapping between a parent template ID and the corresponding item and template types
+/// </summary>
+/// <param name="id">MongoID of the parent item, has to match the id of a valid parent on the server</param>
+/// <param name="item">The item class that should be associated with this parent</param>
+/// <param name="template">The data template class that should be associated with this item</param>
 [AttributeUsage(AttributeTargets.Class)]
-public class CustomParent : Attribute
+public class CustomParent(string id, Type item, Type template) : Attribute
 {
-    public string ParentId { get; }
-    public Type Item { get; }
-    public Type Template { get; }
-
-    public CustomParent(string id, Type item, Type template)
-    {
-        ParentId = id;
-        Item = item;
-        Template = template;
-    }
+    public string ParentId { get; } = id;
+    public Type Item { get; } = item;
+    public Type Template { get; } = template;
 }
