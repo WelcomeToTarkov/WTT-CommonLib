e649ab7194b96ac752f189c9c7e21908d5e26439
peinwastaken <legokid173@gmail.com>
2026-04-25 12:21:06 +0300

    add xml comments for slothelper

--- WTT-ServerCommonLib/Helpers/SlotHelper.cs
+++ WTT-ServerCommonLib/Helpers/SlotHelper.cs
@@ -33,6 +33,17 @@ public class SlotHelper
         throw new IndexOutOfRangeException($"Index on item slot property `{item.Name}` is out of range");
     }
     
+    /// <summary>
+    /// Ensures a slot with the given name exists on the item.
+    /// If the slot doesn't exist, it is created with the provided (optional) parameters
+    /// </summary>
+    /// <param name="item">Item to search the for the slot on</param>
+    /// <param name="slotName">Name of the slot</param>
+    /// <param name="prototype">Prototype id used when creating the slot</param>
+    /// <param name="required">Sets the slot's required value</param>
+    /// <param name="mergeSlotWithChildren">Sets the slot's mergeSlotWithChildren value</param>
+    /// <param name="shift">Sets the slot's shift value</param>
+    /// <returns>Found or newly created slot</returns>
     public Slot EnsureSlot(
         TemplateItem item,
         string slotName,
@@ -74,6 +85,12 @@ public class SlotHelper
         item.Properties.Slots = slots;
         return slot;
     }
+    
+    /// <summary>
+    /// Adds a list of strings to a specified slot
+    /// </summary>
+    /// <param name="slot">Slot to push to</param>
+    /// <param name="ids">Ids to add to the slot's filters</param>
     public void AddIdsToSlot(Slot slot, params string[] ids)
     {
         slot.Properties ??= new SlotProperties();
@@ -95,6 +112,15 @@ public class SlotHelper
         filterContainer.Filter ??= new HashSet<MongoId>();
         filterContainer.Filter.UnionWith(ids.Select(id => (MongoId)id));
     }
+    
+    /// <summary>
+    /// Adds item template ids to a slot's filter.
+    /// The slot must already exist (for example created via <see cref="EnsureSlot"/>)
+    /// </summary>
+    /// <param name="item">Item to push to</param>
+    /// <param name="slotName">Name of the slot</param>
+    /// <param name="ids">List of ids to add to the slot filter</param>
+    /// <exception cref="InvalidOperationException">Thrown if a slot with the specified name does not exist (use <see cref="EnsureSlot"/> to create a slot if needed)</exception>
     public void AddIdsToNamedSlot(
         TemplateItem item,
         string slotName,
@@ -105,6 +131,7 @@ public class SlotHelper
 
         AddIdsToSlot(slot, ids);
     }
+    
     private SlotFilter GetSlotFilterAtIndex(Slot slot, int index)
     {
         var slotFilter = slot.Properties?.Filters?.ToArray() ?? [];
