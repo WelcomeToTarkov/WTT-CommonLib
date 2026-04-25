4aed1ec3216167bc95d447694186a93af7961548
GrooveypenguinX <jakechase2110@gmail.comX>
2026-04-24 21:51:31 -0400

    comments go brrrrrr

--- WTT-ClientCommonLib/Attributes/CustomParentAttribute.cs
+++ WTT-ClientCommonLib/Attributes/CustomParentAttribute.cs
@@ -3,17 +3,29 @@ using System;
 
 namespace WTTClientCommonLib.Attributes;
 
+/// <summary>
+/// Declares a mapping between a parent template ID and the corresponding item and template types
+/// </summary>
+/// <param name="id">MongoID of the parent item, has to match the id of a valid parent on the server</param>
+/// <param name="item">The item class that should be associated with this parent (CAN BE NULL FOR TEMPLATE REGISTRATION)</param>
+/// <param name="template">The data template class that should be associated with this item</param>
 [AttributeUsage(AttributeTargets.Class)]
-public class CustomParent : Attribute
+public class CustomParent(string id, Type item, Type template) : Attribute
 {
     public string ParentId { get; }
-    public Type? Item { get; }
-    public Type? Template { get; }
+    public Type Item { get; }
+    public Type Template { get; }
 
-    public CustomParent(string id, Type? item, Type? template)
+    public class CustomParent : Attribute
     {
-        ParentId = id;
-        Item = item;
-        Template = template;
+        public string ParentId { get; }
+        public Type? Item { get; }
+        public Type? Template { get; }
+
+        public CustomParent(string id, Type? item, Type? template)
+        {
+            ParentId = id;
+            Item = item;
+            Template = template;
+        }
     }
-}
