f073190a6602897088852109c52ed47f016915b4
GrooveypenguinX <grooveypenguinx@gmail.com>
2026-08-01 18:07:38 -0400

    fixed spt bundle loader until 4.1.1 release

---
+++ WTT-ClientCommonLib/Patches/GetBundlePathPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using SPT.Common.Http;
+using SPT.Custom.Models;
+using SPT.Custom.Utils;
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+
+namespace WTTClientCommonLib.Patches;
+
+internal class GetBundlePathPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return AccessTools.Method(typeof(BundleManager), nameof(BundleManager.GetBundlePath));
+    }
+
+    [PatchPrefix]
+    private static bool Prefix(BundleItem bundle, ref string __result)
+    {
+        try
+        {
+            if (!RequestHandler.IsLocal)
+            {
+                __result = "SPT_Runtime/user/cache/bundles/";
+                return false;
+            }
+
+            __result = "SPT_Runtime/" + bundle.ModPath + "/bundles/";
+            return false;
+        }
+        catch (Exception)
+        {
+            return true;
+        }
+    }
+}
