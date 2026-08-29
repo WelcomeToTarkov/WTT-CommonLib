7d3dda4596f0181151d18b0ab3d7a805c635d722
bushtail <evannosich@icloud.com>
2026-08-29 18:16:36 -0400

    Add implicit use target flags instead of repeating UsedImplicitly

--- WTT-ClientCommonLib/Helpers/ExplosionHelper.cs
+++ WTT-ClientCommonLib/Helpers/ExplosionHelper.cs
@@ -13,7 +13,7 @@ namespace WTTClientCommonLib.Helpers;
 /// <summary>
 ///     Helper methods to create explosions using a fake grenade source
 /// </summary>
-[UsedImplicitly]
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public class ExplosionHelper
 {
     /// <summary>
@@ -22,7 +22,6 @@ public class ExplosionHelper
     /// <param name="position">The position to create the explosion at</param>
     /// <param name="originalItem">The item source of the explosion</param>
     /// <param name="owner">The player owner of the explosion</param>
-    [UsedImplicitly]
     public static void DetonateAt(Vector3 position, Item originalItem, IPlayer owner)
     {
         DetonateGrenadeAt(position, new FakeGrenade(3, 7, 90, 80), originalItem, owner);
@@ -38,7 +37,6 @@ public class ExplosionHelper
     /// <param name="maxDist">Maximum explosion distance in meters(?)</param>
     /// <param name="fragCount">The number of fragments the explosion should create</param>
     /// <param name="strength">The strength of the explosion</param>
-    [UsedImplicitly]
     public static void DetonateAt(Vector3 position, Item originalItem, IPlayer owner, float minDist, float maxDist, int fragCount, float strength)
     {
         DetonateGrenadeAt(position, new FakeGrenade(minDist, maxDist, fragCount, strength), originalItem, owner);
@@ -51,7 +49,6 @@ public class ExplosionHelper
     /// <param name="grenade">The fake grenade to detonate</param>
     /// <param name="originalItem">The item source of the explosion</param>
     /// <param name="owner">The player owner of the explosion</param>
-    [UsedImplicitly]
     public static void DetonateGrenadeAt(Vector3 position, FakeGrenade grenade, Item originalItem, IPlayer owner)
     {
         var explosiveAmmoComponent = new ExplosiveAmmoComponent(grenade);
