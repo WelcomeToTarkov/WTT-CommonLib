e4280120a0e82526bdb785380042ff29a5753fe5
bushtail <evannosich@icloud.com>
2026-08-29 17:45:12 -0400

    Add helper methods to create explosions

---
+++ WTT-ClientCommonLib/Components/FakeGrenade.cs
@@ -0,0 +1,50 @@
+﻿using EFT.InventoryLogic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace WTTClientCommonLib.Components;
+
+[method: UsedImplicitly]
+public class FakeGrenade(
+    float fuzeArmTimeSec,
+    float minDist,
+    float maxDist,
+    int fragmentsCount,
+    string fragmentType,
+    string explosionType,
+    bool showHitEffectOnExplode,
+    Vector3 armorDistanceDistanceDamage,
+    Vector3 contusion,
+    Vector3 blindness,
+    bool grenadeComponentIsDummy)
+    : IExplosiveAmmoTemplate
+{
+    public float FuzeArmTimeSec { get; } = fuzeArmTimeSec;
+    public float MinExplosionDistance { get; } = minDist;
+    public float MaxExplosionDistance { get; } = maxDist;
+    public int FragmentsCount { get; } = fragmentsCount;
+    public string FragmentType { get; } = fragmentType;
+    public string ExplosionType { get; } = explosionType;
+    public float ExplosionStrength { get; }
+    public bool ShowHitEffectOnExplode { get; } = showHitEffectOnExplode;
+    public Vector3 ArmorDistanceDistanceDamage { get; } = armorDistanceDistanceDamage;
+    public Vector3 Contusion { get; } = contusion;
+    public Vector3 Blindness { get; } = blindness;
+    public bool GrenadeComponentIsDummy { get; } = grenadeComponentIsDummy;
+
+    public FakeGrenade(float minDist, float maxDist, int fragmentsCount, float strength) : this(0.0f, minDist, maxDist,
+        fragmentsCount, "63b35f281745dd52341e5da7", "Grenade_new", false, new Vector3(1.0f, 4.0f, 25.0f),
+        new Vector3(1.5f, 4.0f, 15.0f), new Vector3(0.0f, 0.0f, 0.0f), false)
+    {
+        ExplosionStrength = strength;
+    }
+
+    public FakeGrenade(float minDist, float maxDist, int fragmentsCount, string fragmentType, string explosionType,
+        float strength) : this(0.0f, minDist, maxDist, fragmentsCount, fragmentType, explosionType, false,
+        new Vector3(1.0f, 4.0f, 25.0f), new Vector3(1.5f, 4.0f, 15.0f), new Vector3(0.0f, 0.0f, 0.0f), false)
+    {
+        ExplosionStrength = strength;
+    }
+}
---
+++ WTT-ClientCommonLib/Helpers/ExplosionHelper.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Comfort.Common;
+using EFT;
+using EFT.Ballistics;
+using EFT.InventoryLogic;
+using JetBrains.Annotations;
+using Systems.Effects;
+using UnityEngine;
+using WTTClientCommonLib.Components;
+
+namespace WTTClientCommonLib.Helpers;
+
+/// <summary>
+///     Helper methods to create explosions using a fake grenade source
+/// </summary>
+[UsedImplicitly]
+public class ExplosionHelper
+{
+    /// <summary>
+    ///     Create an explosion at a given position
+    /// </summary>
+    /// <param name="position">The position to create the explosion at</param>
+    /// <param name="originalItem">The item source of the explosion</param>
+    /// <param name="owner">The player owner of the explosion</param>
+    [UsedImplicitly]
+    public static void DetonateAt(Vector3 position, Item originalItem, IPlayer owner)
+    {
+        DetonateGrenadeAt(position, new FakeGrenade(3, 7, 90, 80), originalItem, owner);
+    }
+    
+    /// <summary>
+    ///     Create an explosion at a given position
+    /// </summary>
+    /// <param name="position">The position to create the explosion at</param>
+    /// <param name="originalItem">The item source of the explosion</param>
+    /// <param name="owner">The player owner of the explosion</param>
+    /// <param name="minDist">Minimum explosion distance in meters(?)</param>
+    /// <param name="maxDist">Maximum explosion distance in meters(?)</param>
+    /// <param name="fragCount">The number of fragments the explosion should create</param>
+    /// <param name="strength">The strength of the explosion</param>
+    [UsedImplicitly]
+    public static void DetonateAt(Vector3 position, Item originalItem, IPlayer owner, float minDist, float maxDist, int fragCount, float strength)
+    {
+        DetonateGrenadeAt(position, new FakeGrenade(minDist, maxDist, fragCount, strength), originalItem, owner);
+    }
+
+    /// <summary>
+    ///     Detonate a fake grenade at a given position.
+    /// </summary>
+    /// <param name="position">The position to create the explosion at</param>
+    /// <param name="grenade">The fake grenade to detonate</param>
+    /// <param name="originalItem">The item source of the explosion</param>
+    /// <param name="owner">The player owner of the explosion</param>
+    [UsedImplicitly]
+    public static void DetonateGrenadeAt(Vector3 position, FakeGrenade grenade, Item originalItem, IPlayer owner)
+    {
+        var explosiveAmmoComponent = new ExplosiveAmmoComponent(grenade);
+        var ballisticsCalculator = Singleton<GameWorld>.Instance.SharedBallisticsCalculator as BallisticsCalculator;
+        var shift = Vector3.up * 0.08f;
+
+        Singleton<Effects>.Instance.EmitGrenade(grenade.ExplosionType, position, Vector3.up);
+        
+        Grenade.Explosion(
+            null,
+            explosiveAmmoComponent,
+            position,
+            owner.ProfileId,
+            ballisticsCalculator,
+            originalItem,
+            shift
+        );
+    }
+}
